12f9e5d495f31afa2c2902b41c606561697dcf2c
Cameron Robson <36795917+Cameronjpr@users.noreply.github.com>
2019-04-09 10:49:13 +0100

    Add files via upload

---
+++ recursion-test.js
@@ -0,0 +1,14 @@
+const factorial = (num) => {
+    if (num <= 1) { return num }
+    else {
+        return num * factorial(num - 1)
+    }
+}
+
+console.log(factorial(0))
+console.log(factorial(1))
+console.log(factorial(2))
+console.log(factorial(3))
+console.log(factorial(4))
+console.log(factorial(5))
+
